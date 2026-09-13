bd07c632d7dba8af33630733c417a35868cf520d
bejoyg-soharstar <bejoy@soharstar.com>
2026-09-13 10:29:40 +0400

    Report from timesheet table

---
+++ src/pages/AttendanceBook.tsx
@@ -0,0 +1,15 @@
+import TimesheetViewer from '../components/TimesheetViewer';
+
+interface AttendanceBookProps {
+  refreshTrigger?: number;
+  onLoadingChange?: (loading: boolean) => void;
+}
+
+export default function AttendanceBook({ refreshTrigger, onLoadingChange }: AttendanceBookProps = {}) {
+  return (
+    <TimesheetViewer
+      refreshTrigger={refreshTrigger}
+      onLoadingChange={onLoadingChange}
+    />
+  );
+}
